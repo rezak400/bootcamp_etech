5d7ff8b4ebceb4951af963eb854d042635014ff0
rezak400 <k400ezza@gmail.com>
2021-01-19 12:56:50 +0700

    add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors:{
+        primary : "#211F21"
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
