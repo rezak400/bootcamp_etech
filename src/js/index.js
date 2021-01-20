339d7db1ae631cde24ae35ca76013141b0a1db96
rezak400 <k400ezza@gmail.com>
2021-01-20 22:46:57 +0700

    add repeat:function

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,26 @@
+//! Function untuk Redirect
 let redirect = (url) => {
     window.location.href = `./${url}.html`
 }
+
+//! Ambil selector dari HTML
+let containerHome = document.getElementById('container');
+let containerCatalog = document.getElementById('container-catalog');
+
+//! Tentukan isi dari div yang akan di ulang
+let catalogHome = `<div onclick="redirect('catalog')" class="hover:opacity-50 h-48  bg-no-repeat bg-center bg-shoe-2 bg-cover cursor-pointer" ></div>`
+let catalog = `<div onclick="redirect('detail')" class="hover:opacity-50 cursor-pointer h-56  bg-no-repeat bg-center bg-shoe-4 bg-cover"></div>`
+
+//! Function untuk repeat element
+let repeat = (element, index, container) => {
+    let lemari = ``
+    for(let i=0; i<index; i++){
+        lemari += element  
+    }
+    container.innerHTML=lemari
+}
+
+//! Function yang akan dijalankan pada body onload()
+let repeatCatalogHome =  (index) => repeat(catalogHome,index,containerHome)
+let repeatCatalog =  (index) => repeat(catalog,index,containerCatalog)
+
